e743dc9376a249c194fec23d57087f46a89284bb
sefacan <sefacanbasoglu67@gmail.com>
2024-09-27 18:51:05 +0300

    us8

--- src/US_308.java
+++ src/US_308.java
@@ -1,2 +1,41 @@
-public class US_308 {
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class US_308 extends BaseDriver {
+    @Test
+    public void test1() {
+        driver.get("https://www.e-junkie.com/ ");
+        MyFunc.Bekle(2);
+
+       // Assert.assertTrue(" Yanlış Url ", driver.getCurrentUrl().equals("https://www.e-junkie.com/"));
+
+        WebElement SeeHowWorks=driver.findElement(By.xpath("//a[@class='blue_btn']"));
+        SeeHowWorks.click();
+        MyFunc.Bekle(2);
+
+
+        MyFunc.Bekle(5);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.elementFromPoint(200, 100).click();");
+
+
+        WebElement iframe = driver.findElement(By.cssSelector("iframe"));
+        driver.switchTo().frame(iframe);
+
+
+        WebElement oynat=driver.findElement(By.cssSelector(".ytp-large-play-button"));
+        oynat.click();
+
+        MyFunc.Bekle(10);
+        BekleKapat();
+
+    }
+
 }
