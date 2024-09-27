f6817922f308a7fe78658922294906cb4e53d72b
alperalpay <alpeay98@gmail.com>
2024-09-27 23:50:07 +0100

    test değişti

--- src/US_305.java
+++ src/US_305.java
@@ -1,102 +1,117 @@
 import Utility.BaseDriver;
-import Utility.MyFunc;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.io.File;
 import java.io.IOException;
 
+import static Utility.BaseDriver.driver;
+import static Utility.BaseDriver.wait;
+
+
 public class US_305 extends BaseDriver {
-    @Test
-    public void Test1() throws AWTException, IOException {
 
-        driver.get("https://shopdemo.fatfreeshop.com/?");
+     @Test
+     public void Test1() throws AWTException, IOException {
 
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='EJProduct_1597355']/.././following-sibling::div/a/div/div/following-sibling::div/button")));
-        WebElement element1 = driver.findElement(By.xpath("//*[@id='EJProduct_1597355']/.././following-sibling::div/a/div/div/following-sibling::div/button"));
-        element1.click();
+          driver.get("https://shopdemo.fatfreeshop.com/?");
 
-        MyFunc.Bekle(2);
+          wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='EJProduct_1597355']/.././following-sibling::div/a/div/div/following-sibling::div/button")));
+          WebElement element1 = driver.findElement(By.xpath("//*[@id='EJProduct_1597355']/.././following-sibling::div/a/div/div/following-sibling::div/button"));
+          element1.click();
 
-        WebElement frame = driver.findElement(By.xpath("//*[@class='EJIframeV3 EJOverlayV3']"));
-        driver.switchTo().frame(frame);
-        WebElement element2 = driver.findElement(By.xpath("//*[@class='Payment-Button CC']"));
-        element2.click();
+          MyFunc.Bekle(2);
 
-
-        WebElement mail = driver.findElement(By.xpath("//*[@placeholder='Email']"));
-        mail.sendKeys("betul@gmail.com");
-
-        WebElement mailconfirm = driver.findElement(By.xpath("//*[@placeholder='Confirm Email']"));
-        mailconfirm.sendKeys("betul@gmail.com");
-
-        WebElement nameoncard = driver.findElement(By.xpath("//*[@placeholder='Name On Card']"));
-        nameoncard.sendKeys("Bet Bet");
-
-        WebElement phone = driver.findElement(By.xpath("//*[@class='Billing-Phone Inline']/input"));
-        phone.sendKeys("555555555");
-
-        WebElement company = driver.findElement(By.xpath("//*[@class='Billing-Company']/input"));
-        company.sendKeys("XXX");
-
-        Robot robot = new Robot();
-        robot.keyPress(KeyEvent.VK_TAB);
-        robot.keyRelease(KeyEvent.VK_TAB);
-        MyFunc.Bekle(2);
-
-        robot.keyPress(KeyEvent.VK_TAB);
-        robot.keyRelease(KeyEvent.VK_TAB);
-        MyFunc.Bekle(2);
-
-        for (int i = 0; i < 8; i++) {
-
-            robot.keyPress(KeyEvent.VK_4);
-            robot.keyRelease(KeyEvent.VK_4);
-            robot.keyPress(KeyEvent.VK_2);
-            robot.keyRelease(KeyEvent.VK_2);
-        }
-
-        MyFunc.Bekle(2);
-
-        robot.keyPress(KeyEvent.VK_1);
-        robot.keyRelease(KeyEvent.VK_1);
-        robot.keyPress(KeyEvent.VK_2);
-        robot.keyRelease(KeyEvent.VK_2);
-
-        robot.keyPress(KeyEvent.VK_2);
-        robot.keyRelease(KeyEvent.VK_2);
-        robot.keyPress(KeyEvent.VK_4);
-        robot.keyRelease(KeyEvent.VK_4);
+          WebElement frame = driver.findElement(By.xpath("//*[@class='EJIframeV3 EJOverlayV3']"));
+          driver.switchTo().frame(frame);
+          WebElement element2 = driver.findElement(By.xpath("//*[@class='Payment-Button CC']"));
+          element2.click();
 
 
-        for (int i = 0; i < 3; i++) {
-            robot.keyPress(KeyEvent.VK_0);
-            robot.keyRelease(KeyEvent.VK_0);
-        }
+          WebElement mail = driver.findElement(By.xpath("//*[@placeholder='Email']"));
+          mail.sendKeys("betul@gmail.com");
 
-        MyFunc.Bekle(2);
-        robot.keyPress(KeyEvent.VK_TAB);
-        robot.keyRelease(KeyEvent.VK_TAB);
-        robot.keyPress(KeyEvent.VK_ENTER);
-        robot.keyRelease(KeyEvent.VK_ENTER);
+          WebElement mailconfirm = driver.findElement(By.xpath("//*[@placeholder='Confirm Email']"));
+          mailconfirm.sendKeys("betul@gmail.com");
 
-        WebElement paybutton = driver.findElement(By.xpath("//*[@class='Pay-Button']"));
-        paybutton.click();
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='download_btn top10']")));
-        WebElement downloadbutton = driver.findElement(By.xpath("//*[@class='download_btn top10']"));
-        downloadbutton.click();
-        MyFunc.Bekle(2);
+          WebElement nameoncard = driver.findElement(By.xpath("//*[@placeholder='Name On Card']"));
+          nameoncard.sendKeys("Bet Bet");
 
-        WebElement element3=driver.findElement(By.xpath("//*[@class='product_name']"));
-        Assert.assertTrue("Doğru Ürün Eklenmedi",element3.getText().contains("Demo"));
+          WebElement phone = driver.findElement(By.xpath("//*[@class='Billing-Phone Inline']/input"));
+          phone.sendKeys("555555555");
 
-        WebElement element4=driver.findElement(By.xpath("//*[@class='all_text content_head2']"));
-        Assert.assertNotNull(element4);
+          WebElement company = driver.findElement(By.xpath("//*[@class='Billing-Company']/input"));
+          company.sendKeys("XXX");
 
-        WebElement element5=driver.findElement(By.xpath("//*[@class='green_text_margin']"));
-        Assert.assertTrue("Order Confirmation is not ok",element5.getText().contains("is confirmed"));
-}
+          Robot robot = new Robot();
+          robot.keyPress(KeyEvent.VK_TAB);
+          robot.keyRelease(KeyEvent.VK_TAB);
+          MyFunc.Bekle(2);
+
+          robot.keyPress(KeyEvent.VK_TAB);
+          robot.keyRelease(KeyEvent.VK_TAB);
+          MyFunc.Bekle(2);
+
+          for (int i = 0; i < 8; i++) {
+
+               robot.keyPress(KeyEvent.VK_4);
+               robot.keyRelease(KeyEvent.VK_4);
+               robot.keyPress(KeyEvent.VK_2);
+               robot.keyRelease(KeyEvent.VK_2);
+          }
+
+          MyFunc.Bekle(2);
+
+          robot.keyPress(KeyEvent.VK_1);
+          robot.keyRelease(KeyEvent.VK_1);
+          robot.keyPress(KeyEvent.VK_2);
+          robot.keyRelease(KeyEvent.VK_2);
+
+          robot.keyPress(KeyEvent.VK_2);
+          robot.keyRelease(KeyEvent.VK_2);
+          robot.keyPress(KeyEvent.VK_4);
+          robot.keyRelease(KeyEvent.VK_4);
+
+
+          for (int i = 0; i < 3; i++) {
+               robot.keyPress(KeyEvent.VK_0);
+               robot.keyRelease(KeyEvent.VK_0);
+          }
+
+          MyFunc.Bekle(2);
+          robot.keyPress(KeyEvent.VK_TAB);
+          robot.keyRelease(KeyEvent.VK_TAB);
+          robot.keyPress(KeyEvent.VK_ENTER);
+          robot.keyRelease(KeyEvent.VK_ENTER);
+
+          WebElement paybutton = driver.findElement(By.xpath("//*[@class='Pay-Button']"));
+          paybutton.click();
+          wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='download_btn top10']")));
+          WebElement downloadbutton = driver.findElement(By.xpath("//*[@class='download_btn top10']"));
+          downloadbutton.click();
+          MyFunc.Bekle(2);
+
+          WebElement element3=driver.findElement(By.xpath("//*[@class='product_name']"));
+          Assert.assertTrue(element3.getText().contains("Demo"), "Doğru Ürün Eklenmedi");
+
+          WebElement element4=driver.findElement(By.xpath("//*[@class='all_text content_head2']"));
+          Assert.assertNotNull(element4);
+
+          WebElement element5=driver.findElement(By.xpath("//*[@class='green_text_margin']"));
+          Assert.assertTrue(element5.getText().contains("is confirmed"), "Order Confirmation is not ok");
+
+
+          }
+
+
+     }
